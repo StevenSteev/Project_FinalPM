f50531bb7d791408edc10179534af8591bfcb824
StevenSteev <201804788h@utea.edu.pe>
2022-12-22 23:33:00 -0500

    Insetando librerias para la app

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,34 @@ import 'package:peliculas/screens/screens.dart';
 import 'package:provider/provider.dart';
 
 import 'package:peliculas/providers/movies_provider.dart';
- 
+
 void main() => runApp(AppState());
 
-
 class AppState extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: ( _ ) => MoviesProvider(), lazy: false ),
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false),
       ],
       child: MyApp(),
     );
   }
 }
 
-
- 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Películas',
-      initialRoute: 'home',
+      initialRoute: 'hom',
       routes: {
-        'home':    ( _ ) => HomeScreen(),
-        'details': ( _ ) => DetailsScreen(),
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
       },
-      theme: ThemeData.light().copyWith(
-        appBarTheme: AppBarTheme(
-          color: Colors.indigo
-        )
-      ),
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: AppBarTheme(color: Colors.indigo)),
     );
   }
 }

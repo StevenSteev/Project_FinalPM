f68284e911328d39779bd94838cc2f29ceba4137
StevenSteev <201804788h@utea.edu.pe>
2022-12-25 12:06:47 -0500

    Insertando de peliculas en español

---
+++ lib/providers/movie_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas/models/models.dart';
+import 'dart:convert';
+
+import 'package:peliculas/models/now_playing_response.dart';
+import 'package:peliculas/models/search_response.dart';

d90b55091858d8bdff294035560198de8fed75ae
StevenSteev <201804788h@utea.edu.pe>
2022-12-25 12:10:46 -0500

    Insertando proveedor de peliculas en español

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -6,3 +6,22 @@ import 'dart:convert';
 
 import 'package:peliculas/models/now_playing_response.dart';
 import 'package:peliculas/models/search_response.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  String _baseURL = 'api.themoviedb.org';
+  String _apiKey = '8d6ed2ec2251fc94ee265133935c8d8f';
+  String _language = 'es-ES';
+  // Lista donde se almacenará pelis
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+  // Para incrementar en 1 el scroll del POPULAR
+  int _popularPage = 0;
+  // Lista de actores Id:Peli
+  Map<int, List<Cast>> moviesCast = {};
+
+  MoviesProvider() {
+    print('Movie Provider inicializado');
+    // This (opcional), metodo se llama
+    this.getOnDisplayMovie();
+    this.getPopularMovie();
+  }
